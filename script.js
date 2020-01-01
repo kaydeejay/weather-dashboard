92e27bf5d0c4811787809a76ac57b613eec30c07
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-01-01 16:06:18 -0500

    more html skeleton, set weather api call

---
+++ script.js
@@ -0,0 +1,5 @@
+var weatherKey = "7fa09e46f60ddbd49aa136e8c0cd30a4";
+var weatherCityName = "New York";
+var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + weatherCityName + "&appid=" + weatherKey;
+
+console.log(weatherURL);
