2f2f044dc20241cab4b6cc053679bc9ad2fe12c6
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-01-02 10:40:20 -0500

    create recent city search list

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-var weatherKey = "7fa09e46f60ddbd49aa136e8c0cd30a4";
-var weatherCityName = "New York";
-var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + weatherCityName + "&appid=" + weatherKey;
+$(document).ready(function(){
 
-console.log(weatherURL);
+    var weatherKey = "7fa09e46f60ddbd49aa136e8c0cd30a4";
+    var weatherCityName = "New York";
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + weatherCityName + "&appid=" + weatherKey;
+    var searchForm = $(".city-search");
+
+    $(searchForm).submit(addCity);
+
+    function addCity(){
+        event.preventDefault();
+        var cityList = $(".list-group");
+        var newCity = $("#cityName").val();
+        var newCityEl = $("<li>");
+
+        $(newCityEl).attr("class", "list-group-item");
+        $(newCityEl).text(newCity);
+        $(cityList).prepend(newCityEl);
+
+        $("#cityName").val("");
+    }
+});
